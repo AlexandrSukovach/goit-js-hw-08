4aa03c04afa05cdb26e1f0ffeea37d7120986cd4
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-09 20:22:57 +0200

    task-03 bcghfdktybt

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
 
+window.onload = () => {
+   // находим элемент <video>
+   let video = document.querySelector('#vimeo-player')
+
+   // если localStorage содержит значение currentTime (текущее время), присваиваем это значение video.currentTime
+   if (localStorage.currentTime) {
+      video.currentTime = localStorage.currentTime
+   }
+
+   // при каждом изменении video.currentTime, записываем его значение в localStorage.currentTime
+   video.addEventListener('timeupdate', () => localStorage.currentTime = video.currentTime)
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,19 +5,19 @@ const STORAGE_KEY = 'feedback-form-state';
 const refs = {
    form: document.querySelector('.feedback-form'),
    textarea: document.querySelector('.feedback-form  textarea'),
+   inputEmail: document.querySelector('.feedback-form  input'),
 };
-
 
 const formData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 50));
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
 populateTextarea();
 
 function onFormSubmit(evt) {
    evt.preventDefault();
-   // console.log('Отправляем форму');
+   console.log('Отправляем форму');
    console.log(formData);
    evt.currentTarget.reset();
    localStorage.removeItem(STORAGE_KEY);
@@ -39,10 +39,11 @@ function onTextareaInput() {
 
 function populateTextarea() {
    const savedMessage = localStorage.getItem(STORAGE_KEY);
-
    if (savedMessage) {
-      refs.textarea.value = savedMessage;
-      console.log(savedMessage)
+      let obMessage = JSON.parse(savedMessage);
+      refs.textarea.value = obMessage.message;
+      refs.inputEmail.value = obMessage.email;
+      // console.log(obMessage)
    }
 
 }
